a91f8faa30621a4be95c0ec9acf6b04c0df635ae
J-P-Q <que.johnn0@gmail.com>
2026-03-07 17:01:27 +0800

    Uploaded Files from Project

---
+++ versions/Ver1_BotPolling.asm
@@ -0,0 +1,224 @@
+LIST P=16F877A
+#include <P16F877A.inc>
+
+__CONFIG _CP_OFF & _WDT_OFF & _PWRTE_ON & _HS_OSC & _LVP_OFF
+
+
+CBLOCK 0x20		
+	delay1
+	delay2
+
+	tempPortB
+
+	; ---- Direction Vars
+	forward_both
+	
+	HR_R
+	HR_L
+
+	SR_R
+	SR_L
+
+	HL_R
+	HL_L
+	
+	SL_R
+	SL_L
+
+ENDC
+
+ORG 0x00
+	GOTO MAIN
+
+
+MAIN:	;=====================MAIN
+	;=======BANK1 Select
+	
+	BCF STATUS, RP1
+	BSF STATUS, RP0
+	
+	;------------TRIS
+	CLRF TRISC
+	CLRF TRISD			;for testing
+
+	MOVLW 0xFF
+	MOVWF TRISB	
+	
+	;------------ADCON (set as digital)
+	MOVLW 0x07
+	MOVWF ADCON1
+	
+
+	;---------------RB4 to 6 
+	BCF OPTION_REG, 7		;enable pullup bit 7 = 0
+
+
+	; -------------------PWM INIT
+	MOVLW 165	; PR2 = [Fosc /(4 * F_PWM * prescale)] - 1
+	MOVWF PR2	; PR2 = [  8M /(4 * 3k    * (1 4 or 16)] - 1
+
+
+
+	;=======BANK0 Select
+	BCF STATUS, RP0
+
+	;-----------PORTs
+	CLRF PORTB
+	CLRF PORTC
+	
+	;-------------------PWM
+	CLRF CCPR1L 	; 8 MSBs of duty cycle
+	CLRF CCPR2L
+
+	MOVLW 0x06	
+	MOVWF T2CON
+
+	MOVLW 0x0C
+	MOVWF CCP1CON
+	MOVWF CCP2CON
+
+	; ------------- define direction variables
+	MOVLW 60							; EDIT_THIS
+	MOVWF forward_both
+	
+	MOVLW 0							; EDIT THIS
+	MOVWF HR_R
+	MOVLW 120							; EDIT THIS
+	MOVWF HR_L
+
+	MOVLW 30							; EDIT THIS
+	MOVWF SR_R 
+	MOVLW 90							; EDIT THIS
+	MOVWF SR_L
+
+	MOVLW 120							; EDIT THIS
+	MOVWF HL_R
+	MOVLW 0							; EDIT THIS
+	MOVWF HL_L	
+	
+	MOVLW 90							; EDIT THIS
+	MOVWF SL_R
+	MOVLW 30							; EDIT THIS
+	MOVWF SL_L
+
+	CLRF tempPortB
+	
+
+LOOP:	;=================================LOOP
+	MOVF PORTB, W
+	MOVWF tempPortB
+	MOVLW 0x8F 		;1000 1111
+	IORWF tempPortB, F
+	
+
+	; --------------- 3 IR sensor Checker
+	; W B W bit 6 5 4		1 means black
+	MOVLW 0xAF	;X010 XXXX
+	XORWF tempPortB, W
+	BTFSC STATUS, Z
+	CALL FORWARD_Wheels
+	
+	
+	; w W B
+	MOVLW 0x9F	;X001 XXXX
+	XORWF tempPortB, W
+	BTFSC STATUS, Z
+	CALL HARD_RIGHT	
+	
+	; WBB
+	MOVLW 0xBF	;X011 XXXX
+	XORWF tempPortB, W
+	BTFSC STATUS, Z
+	CALL SOFT_RIGHT	
+
+	;BWW
+	MOVLW 0xCF	;X100 XXXX
+	XORWF tempPortB, W
+	BTFSC STATUS, Z
+	CALL HARD_LEFT	
+
+	;BBW
+	MOVLW 0xEF	;X110 XXXX
+	XORWF tempPortB, W
+	BTFSC STATUS, Z
+	CALL SOFT_LEFT	
+
+	;BBB
+	MOVLW 0xFF	;X111 XXXX
+	XORWF tempPortB, W
+	BTFSC STATUS, Z
+	CALL STOP
+
+	;WWW
+	MOVLW 0x8F	;X000 XXXX
+	XORWF tempPortB, W
+	BTFSC STATUS, Z
+	CALL STOP
+	GOTO LOOP
+
+;==================FUNCTIONS
+
+;--------- Let CCPR1L be right, CCPR2L be left 
+FORWARD_Wheels:
+	MOVF forward_both, W
+	MOVWF CCPR1L
+	MOVWF CCPR2L
+	RETURN
+
+HARD_RIGHT:
+	MOVF HR_R, W
+	MOVWF CCPR1L
+
+	MOVF HR_L, W
+	MOVWF CCPR2L
+	RETURN
+
+SOFT_RIGHT:
+	MOVF SR_R, W
+	MOVWF CCPR1L
+
+	MOVF SR_L, W
+	MOVWF CCPR2L
+	RETURN
+
+HARD_LEFT:
+	MOVF HL_R, W
+	MOVWF CCPR1L
+
+	MOVF HL_L, W
+	MOVWF CCPR2L
+	RETURN
+
+SOFT_LEFT:
+	MOVF SL_R, W
+	MOVWF CCPR1L
+
+	MOVF SL_L, W
+	MOVWF CCPR2L
+	RETURN
+
+STOP:	
+	CLRF CCPR1L
+	CLRF CCPR2L
+	RETURN
+
+
+
+
+;----------------DELAY
+DELAY:
+	MOVLW 0xFF
+	MOVWF delay2
+
+OUTER_DELAY:
+	MOVLW 0xFF
+	MOVWF delay1
+INNER_DELAY:
+	DECFSZ delay1
+	GOTO INNER_DELAY
+
+	DECFSZ delay2
+	GOTO OUTER_DELAY	
+	RETURN
+
+END
